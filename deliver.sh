a0b10e32d2463c6bcb728fc0bf5da87ed4cf9598
aspirantioner <759435690@qq.com>
2023-04-10 22:56:34 -0700

    push

--- deliver.sh
+++ deliver.sh
@@ -2,13 +2,13 @@
 #!/bin/sh
 #write your shell cmd:
 
-if [[ $1 -eq "push" ]]
+if [ "$1" == "push" ]
 then
 git add .
 git commit -m "push"
 git push $2 master --force
 else
-if [[ $1 -eq "pull" ]]
+if [ "$1" == "pull" ]
 then
 git pull --force $2 master:master
 fi
